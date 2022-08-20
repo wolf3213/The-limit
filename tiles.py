c3aac49c81794e05f3d6878b945d25bc8fbb1d09
wolf3213 <45437300+wolf3213@users.noreply.github.com>
2022-08-20 19:16:53 +0200

    Add files via upload

--- tiles.py
+++ tiles.py
@@ -9,4 +9,26 @@ class Tile(pygame.sprite.Sprite):
 
 	def update(self,x_shift):
 		self.rect.x += x_shift
+class Wind_(pygame.sprite.Sprite):
+	def __init__(self,pos):
+		super().__init__()
+		self.image = pygame.image.load('graphics/wind.png').convert_alpha()
+		self.rect = self.image.get_rect(topleft = pos)
 
+	def update(self,x_shift):
+		self.rect.x += x_shift
+class Windleft_(pygame.sprite.Sprite):
+	def __init__(self,pos):
+		super().__init__()
+		self.image = pygame.image.load('graphics/wind_left.png').convert_alpha()
+		self.rect = self.image.get_rect(topleft = pos)
+
+	def update(self,x_shift):
+		self.rect.x += x_shift
+class Platform_(pygame.sprite.Sprite):
+	def __init__(self, pos):
+		super().__init__()
+		self.image = pygame.image.load('graphics/platform_3.png').convert_alpha()
+		self.rect = self.image.get_rect(topleft=pos)
+	def update(self, x_shift):
+		self.rect.x += x_shift
